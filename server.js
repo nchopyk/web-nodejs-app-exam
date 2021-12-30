d3bdc54b25c9b806e934cfaac6928c1afe6433f9
Nazar Chopyk <nazar.chopyk@gmail.com>
2021-12-30 17:16:43 +0200

    Removed unnecessary path part

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', weatherRouter);
+app.use('/', weatherRouter);
 
 app.get('*', function(req, res){
   res.status(404).send('Page not found...');
